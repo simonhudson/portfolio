6426a50c6b4790eb814b583534ed819b4d6c6d03
Simon Hudson <simon.hudson@experian.com>
2015-12-18 08:34:16 +0000

    1) Layout tweaks 2) Add pull classes to grid

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <?php foreach($portfolio as $item): ?>
         <section class="portfolio__item" id="item-<?= $item->slug; ?>">
             <div class="grid__wrap portfolio__content">
-                <div class="grid__span--full center-content">
+                <div class="grid__span--6 center-content">
                     <img alt="<?= $item->title; ?> screen shot" class="portfolio__img" src="<?= $paths->imgs; ?>portfolio__<?= $item->slug; ?>--sm.png" srcset="<?= $paths->imgs; ?>portfolio__<?= $item->slug; ?>--md.png 1000w" />
                 </div>
-                <div class="grid__span--12">
+                <div class="grid__span--10 grid__pull-right--2">
                     <h2 class="portfolio__title"><?= $item->title; ?></h2>
                     <?php if (isset($item->client)): ?>
                         <p class="portfolio__client"><strong><?= $item->client; ?></strong></p>
@@ -23,22 +23,22 @@
                     <?php if (isset($item->text)): ?>
                         <p><?= $item->text; ?></p>
                     <?php endif; ?>
-                </div>
-                <div class="grid__span--6">
-                    <?php if (isset($item->url) || isset($item->github)) { ?>
-                        <?php if (isset($item->url)): ?>
-                            <a class="btn portfolio-action__link" href="http://<?= $item->url; ?>" target="_blank">
-                                <span class="fa fa-link margin-r-sm"></span>
-                                View project<span class="hidden"> (Link opens in a new window)</span>
-                            </a>
-                        <?php endif; ?>
-                        <?php if (isset($item->github)): ?>
-                            <a class="btn--secondary portfolio-action__link" href="http://www.github.com/simonhudson/<?= $item->github; ?>" target="_blank">
-                                <span class="fa fa-github margin-r-sm"></span>
-                                View on Github<span class="hidden"> (Link opens in a new window)</span>
-                            </a>
-                        <?php endif; ?>
-                    <?php } ?>
+                    <?php if (isset($item->url) || isset($item->github)): ?>
+                        <div class="portfolio-action__wrap">
+                            <?php if (isset($item->url)): ?>
+                                <a class="btn portfolio-action__link" href="http://<?= $item->url; ?>" target="_blank">
+                                    <span class="fa fa-link fa-2x margin-r-sm"></span>
+                                    View project<span class="hidden"> (Link opens in a new window)</span>
+                                </a>
+                            <?php endif; ?>
+                            <?php if (isset($item->github)): ?>
+                                <a class="btn--secondary portfolio-action__link" href="http://www.github.com/simonhudson/<?= $item->github; ?>" target="_blank">
+                                    <span class="fa fa-github fa-2x margin-r-sm"></span>
+                                    View on Github<span class="hidden"> (Link opens in a new window)</span>
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </section>
