3c370b3f922c11571f97d1a259c52a974643a717
Simon Hudson <simon.hudson@experian.com>
2015-12-21 07:32:04 +0000

    Add portfolio items

--- includes/portfolio.inc.php
+++ includes/portfolio.inc.php
@@ -17,6 +17,20 @@ $portfolio = (object) array(
     //      'Swig'
     //  )
     // ),
+    'lastfm' => (object) array(
+        'slug'      => 'lastfm',
+        'title'     => 'LastFM',
+        'client'    => 'Personal project',
+        'text'      => '',
+        'url'       => 'lastfm.heyhudson.com',
+        'github'    => 'lastfm',
+        'madewith'  => (object) array(
+            'XHTML',
+            'CSS',
+            'JavaScript/jQuery',
+            'LastFM API'
+        )
+    ),
     'gov-petitions' => (object) array(
         'slug'      => 'gov-petitions',
         'title'     => 'UK Government Petitions',
@@ -36,7 +50,7 @@ $portfolio = (object) array(
         'slug'      => 'steve-bloomers-watching',
         'title'     => 'Steve Bloomer\'s Watching',
         'client'    => 'Personal project',
-        'text'      => 'An experiment using the publicly-accessible government petitions API. Petitions are searchable by ID, data is returned to the browser and displayed using the KnockoutJS framework. The API is polled periodically to give a real-time update of the number of signatories.',
+        'text'      => 'As a season ticket holder at Derby County FC (for my sins!), I decided to create a blog in which to write match reports and articles on the club. The blog uses the basic WordPress engine which has been ripped out and integrated into a custom <abbr title="Hypertext Markup Language">HTML</abbr>5 template. The site also makes use of <abbr title="Cascading Style Sheets">CSS</abbr> media queries to enable a responsive layout across devices.',
         'url'       => 'www.stevebloomerswatching.com',
         'github'    => '',
         'madewith'  => (object) array(
