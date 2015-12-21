0ad3ca535690ca9e78065ecea504cf8c0a389c04
Simon Hudson <simon.hudson@experian.com>
2015-12-21 08:45:14 +0000

    1) Add footer 2) Add filesize func 3) Add CV

--- functions/functions.inc.php
+++ functions/functions.inc.php
@@ -3,5 +3,6 @@
 include('fn.currentUrl.php');
 include('fn.currentPage.php');
 include('fn.pageHeading.php');
+include('fn.fileSize.php');
 
 ?>
--- layouts/postcontent.inc.php
+++ layouts/postcontent.inc.php
@@ -1,3 +1,22 @@
     </main>
+
+    <footer class="footer--global">
+        <div class="grid__wrap">
+            <div class="about-text grid__span--12">
+                <img alt="" src="/assets/imgs//self-london.png" />
+                <h2>Hi there, my name's Simon and I build websites.</h2>
+                <p>I am an experienced web designer/front-end developer who specialises in writing lean, standards-compliant <abbr title="eXtensible Hypertext Markup Language">(X)HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr> and JavaScript to create accessible and usable websites. I have a strong understanding of <abbr title="Web Content Accessibility Guidelines">WCAG</abbr> and <abbr title="Web Accessibility Initiative">WAI</abbr> requirements, W3C standards, browser quirks and progressive enhancement/graceful degradation, as well as experience developing for assistive technologies.</p>                </div>
+            <div class="links grid__span--6">
+                <p>
+                    <a class="btn center-content" href="/Simon_Hudson_CV.pdf">
+                        <span class="fa fa-file-pdf-o fa-2x margin-r-sm"></span>
+                        <span class="">Download my CV</span>
+                        <span>(PDF, <?= formatBytes(filesize('Simon_Hudson_CV.pdf')); ?>)</span>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
