2a90116dec8a458cc5993e8f2ddc729016bae39e
Simon Hudson <simon.hudson@experian.com>
2015-12-23 07:03:45 +0000

    Footer img spacing

--- layouts/postcontent.inc.php
+++ layouts/postcontent.inc.php
@@ -3,7 +3,7 @@
     <footer class="footer--global center-content">
         <div class="grid__wrap">
             <div class="grid__span--10 center">
-                <img alt="Simon Hudson" class="img-circle" src="<?= $paths->imgs; ?>self.jpg" />
+                <img alt="Simon Hudson" class="img-circle self-img" src="<?= $paths->imgs; ?>self.jpg" />
                 <h2>Hi there, my name's Simon and I build websites.</h2>
                 <p>I am an experienced web designer/front-end developer who specialises in writing lean, standards-compliant <abbr title="eXtensible Hypertext Markup Language">(X)HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr> and JavaScript to create accessible and usable websites. I have a strong understanding of <abbr title="Web Content Accessibility Guidelines">WCAG</abbr> and <abbr title="Web Accessibility Initiative">WAI</abbr> requirements, W3C standards, browser quirks and progressive enhancement/graceful degradation, as well as experience developing for assistive technologies.</p>
             <div class="btn-row">
