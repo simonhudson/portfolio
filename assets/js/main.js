2791aeaf8bd204887662c7a60f3fd6d2d4a1dab4
Simon Hudson <simon.hudson@experian.com>
2017-05-23 10:08:09 +0100

    Update JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-function headerScroll() {
-	var header = document.querySelector('.js-header');
-	var headerHeight = header.offsetHeight;
-	var method = (window.scrollY >= headerHeight) ? 'add' : 'remove';
+const headerScroll = () => {
+	const header = document.querySelector('.js-header');
+	const headerHeight = header.offsetHeight;
+	const method = (window.scrollY >= headerHeight) ? 'add' : 'remove';
 	header.classList[method]('is-scrolled');
-}
+};
 window.addEventListener('scroll', headerScroll);
