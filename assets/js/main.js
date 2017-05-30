5ad62659178ba489fa2dc40482dd2b646cc22a0e
Simon Hudson <simon.hudson@experian.com>
2017-05-30 12:16:04 +0100

    Header updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
+// Add hs-js class to page
+const hasJs = () => document.querySelector('html').classList.add('has-js');
+hasJs();
+
+// Check JS
+const checkJs = () => document.querySelector('html').classList.contains('has-js');
+
+// Set scrolled header
 const headerScroll = () => {
 	const header = document.querySelector('.js-header');
+	if (!header || !checkJs()) return;
 	const headerHeight = header.offsetHeight;
 	const method = (window.scrollY >= headerHeight) ? 'add' : 'remove';
 	header.classList[method]('is-scrolled');
