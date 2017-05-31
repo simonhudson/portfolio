7464d68c14f6f3ff03c3ba5d194eaeb86140c600
Simon Hudson <simon.hudson@experian.com>
2017-05-31 09:11:02 +0100

    Remove superfluous vars from isScrolled

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,6 @@ const checkJs = () => document.querySelector('html').classList.contains('has-js'
 // Set scrolled header
 const isScrolled = () => {
 	const html = document.querySelector('html');
-	const header = document.querySelector('.js-header');
-	if (!header || !checkJs()) return;
-	const headerHeight = header.offsetHeight;
 	const method = (window.scrollY > 0) ? 'add' : 'remove';
 	html.classList[method]('is-scrolled');
 };
